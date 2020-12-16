44f4cf9b12cfbdda841c617aff5ae19d5d49ed39
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2020-12-16 14:36:26 +0100

    Lifecycle React (filmDetail)

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -12,3 +12,10 @@ export function getFilmsFromApiWithSearchedText(text, page) {
 export function getImageFromApi(name) {
     return 'https://image.tmdb.org/t/p/w300' + name
 }
+
+// Récupération du détail d'un film
+export function getFilmDetailFromApi (id) {
+    return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+        .then((response) => response.json())
+        .catch((error) => console.error(error));
+    }
