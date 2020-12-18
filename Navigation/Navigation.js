9a78a9e54e6d9fe4511513bea93e622509cbcdfd
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2020-12-18 15:57:43 +0100

    upgrading from 4 to 5 + icons (ReactNavigation)

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -8,6 +8,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Search from '../Components/Search'
 import FilmDetail from '../Components/FilmDetail'
 import Favorites from '../Components/Favorites';
+import { Image, StyleSheet } from 'react-native';
 
 
 const Stack = createStackNavigator();
@@ -25,13 +26,44 @@ const SearchStackNavigator = () => (
 export default () => (
             <NavigationContainer>
                 <Tab.Navigator>
-                    <Tab.Screen name="Search" component={SearchStackNavigator} />
-                    <Tab.Screen name="Favorites" component={Favorites} />
+                    <Tab.Screen 
+                        name="Search" 
+                        component={SearchStackNavigator}
+                        options={{tabBarIcon: () => {
+                            return <Image source={require('../Images/ic_search.png')} style={styles.icon}/>
+                        }}} 
+                        tabBarOptions={{
+                            activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+                            inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+                            showLabel: false, // On masque les titres
+                            showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+                        }}
+                    
+                    />
+                    <Tab.Screen 
+                        name="Favorites" 
+                        component={Favorites}
+                        options={{tabBarIcon: () => {
+                            return <Image source={require('../Images/ic_favorite_border.png')} style={styles.icon}/>
+                        }}} 
+                        tabBarOptions={{
+                            activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+                            inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+                            showLabel: false, // On masque les titres
+                            showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+                        }}
+                    
+                    />
                 </Tab.Navigator>
         </NavigationContainer>
 )
-    
 
+const styles = StyleSheet.create({
+    icon: {
+        width: 30,
+        height: 30
+    }
+  })
 
 
 
